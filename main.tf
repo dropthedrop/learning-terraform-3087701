097cf1ecb4a77833eae2051a5ec1dd22c602b2b4
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-04 08:24:15 +1100

    load balancer

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-target_groups = [
+ target_groups = [
     {
    
       name_prefix      = "blog"
