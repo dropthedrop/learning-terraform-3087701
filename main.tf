8a6064bb3d3cb1bdb36376af868c53c660a463e9
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-03 22:41:49 +1100

    Create new Dev vpc

--- main.tf
+++ main.tf
@@ -50,10 +50,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id  = module.blog_vpc.vpc_id
-  name = "blog"
+  
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
