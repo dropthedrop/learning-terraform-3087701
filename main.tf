755babbc028798415f825482c6235d8afbc20651
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-03 21:53:38 +1100

    create security group

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "blog" {
 vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
