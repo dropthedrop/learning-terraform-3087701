3f1fa35511c1ac6ff1cae6d49c8c8c9496adefa7
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-04 19:44:03 +1100

    22

--- variables.tf
+++ variables.tf
@@ -2,3 +2,55 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+ variable "ami-filter" {
+   description = "Name filter and owner for AMI"
+
+   type = oject({
+    name  = string
+    owner = string
+   })
+
+default  = {
+  name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  owner  = "979382823631" # Bitnami
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+variable "environment" {
+description = "Development Environment"
+
+  type  = object ({
+    name = string
+    network_prefix = string
+  })
+
+  default = {
+  name = "dev"
+  network_prefix = "10.0"
+  }
+}
+
+
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform    = "true"
+    Environment = "dev"
+  }
+}
+
+variable "asg_min_size" {
+  description = "Minimum number of instances in the ASG"
+  default = 1
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
+}
+
+ 
