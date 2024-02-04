b1fd7b7bdd4f1f0c849646e8570c5fa3229a8cfc
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-04 20:01:23 +1100

    22

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -15,17 +15,7 @@ default  = {
   owner  = "979382823631"
 }
 
-data {"aws_vpc" "default"
-  default = true
-}
 
-variable "environment" {
-description = "Development Environment"
-
-  type  = object ({
-    name = string
-    network_prefix = string
-  })
 
   default = {
   name = "dev"
@@ -44,12 +34,10 @@ description = "Development Environment"
 }
 
 variable "asg_min_size" {
-  description = "Minimum number of instances in the ASG"
   default = 1
 }
 
 variable "asg_max_size" {
-  description = "Maximum number of instances in the ASG"
   default = 2
 }
 
