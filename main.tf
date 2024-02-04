3ba3b19615d7f57ba58eb193d7219be53903e942
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-04 18:43:40 +1100

    2

--- main.tf
+++ main.tf
@@ -38,16 +38,14 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.3.1"
   
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-
+  name                = "blog"
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.alb_target_group_id
+  target_group_arns   = module.blog_alb.alb_target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
-  image_id           = data.aws_ami.app_ami.id
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 }
 
@@ -70,7 +68,7 @@ module "autoscaling" {
 }
 
 output "alb_target_group_arns" {
-  value = module.blog_alb.target_group_arns
+  value = module.blog_alb.alb_target_group_arns
 }
 
 
