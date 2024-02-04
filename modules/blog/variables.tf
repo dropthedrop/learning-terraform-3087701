f57e3797c775eadee47141f6e441e450265c5107
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-04 20:03:57 +1100

    h

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -17,21 +17,16 @@ default  = {
 
 
 
-  default        = {
+  default         {
   name           = "dev"
   network_prefix = "10.0"
   }
 }
 
 
-  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  servers            = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  tags = {
-    Terraform    = "true"
-    Environment = "dev"
-  }
-}
 
 variable "asg_min_size" {
   default = 1
