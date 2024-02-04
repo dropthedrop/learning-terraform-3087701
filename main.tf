bd57a8e6d48dd615b4868d6a631884398ecc0970
dropthedrop <35862035+dropthedrop@users.noreply.github.com>
2024-02-04 19:45:57 +1100

    22

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.3.1"
 
-  name = "${var.environment.name}-blog"
+  name = "blog"
   min_size = var.asg_min_size
   max_size = var.asg_max_size
 
@@ -48,7 +48,7 @@ module "autoscaling" {
   instance_type    = var.instance_type
 }
 
-module "${var.environment.name}-blog_alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name             = "blog"
@@ -58,7 +58,7 @@ module "${var.environment.name}-blog_alb" {
 
   target_groups = {
     ex-instance = {
-      name_prefix = "${var.environment.name}"
+      name_prefix = "blog"
       protocol    = "HTTP"
       port        = 80
       target_type = "instance"
@@ -70,7 +70,7 @@ module "${var.environment.name}-blog_alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "${var.environment.name}-blog"
+  name    = "blog"
 
   vpc_id               = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
